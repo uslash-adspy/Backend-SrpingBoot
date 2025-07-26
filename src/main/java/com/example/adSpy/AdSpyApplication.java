2ae01ec8c294d58b63e37b5077d6db9d4d9060a1
Shuung <qmoy2510@gmail.com>
2025-07-26 16:20:57 +0900

    프로젝트 생성

---
+++ src/main/java/com/example/adSpy/AdSpyApplication.java
@@ -0,0 +1,13 @@
+package com.example.adSpy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AdSpyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AdSpyApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/adSpy/AdSpyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.adSpy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AdSpyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
